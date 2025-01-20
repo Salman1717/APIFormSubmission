cc8bd1d462e9ed79fff9d469945d3ee092370174
Salman Mhaskar <salmanmhaskar17@gmail.com>
2025-01-20 17:59:28 +0530

    Initial Commit

---
+++ ApiFormSubmission/ApiFormSubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiFormSubmissionApp.swift
+//  ApiFormSubmission
+//
+//  Created by Salman Mhaskar on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ApiFormSubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiFormSubmission/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApiFormSubmission
+//
+//  Created by Salman Mhaskar on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
